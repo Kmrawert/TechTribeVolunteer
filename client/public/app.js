53da61e7fd3f5588a35237d721941492309b09f7
molly02 <molly_patterson@yahoo.com>
2019-11-05 17:26:17 -0600

    added initial database code

---
+++ client/public/app.js
@@ -0,0 +1,23 @@
+
+function displayResults(events) {
+    // First, empty the table
+    $("tbody").empty();
+  
+    // Then, for each entry of that json...
+    events.forEach(function(event) {
+      // match with what taylor has in html
+      var tr = $("<tr>").append(
+        $("<td>").text(event.name),
+        $("<td>").text(event.description),
+        $("<td>").text(event.link),
+      );
+  
+      $("tbody").append(tr);
+    });
+  }
+
+  // First thing: ask the back end for json with all events
+  $.getJSON("/all", function(data) {
+    // Call our function to generate a table body
+    displayResults(data);
+  });
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const path = require("path");
+const mongojs = require("mongojs");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
+
+const events = require("./eventsModel.js");
+
+mongoose.connect("mongodb://localhost/volunteer", { useNewUrlParser: true });
+
+// data into the array?
+var data = {
+  array: ["item1", "item2", "item3"],
+  boolean: false,
+  string:
+    "\"Don't worry if it doesn't work right. If everything did, you'd be out of a job\" - Mosher's Law of Software Engineering",
+  number: 42
+};
+// 
+
+const databaseUrl = "volunteer";
+const collections = ["users"];
+const db = mongojs(databaseUrl, collections);
+// list all collections here or diff for each table?
+
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+events.create(data)
+  .then(function(dbEvents) {
+    // If saved successfully, print the new Example document to the console
+    console.log(dbEvents);
+  })
+  .catch(function(err) {
+    console.log(err.message);
+  });
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +54,17 @@ app.use("/api", apiRoutes);
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.get("/events", function(req, res) {
+  db.events.find({}, function(err, found) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      res.json(found);
+    }
+  });
 });
 
 app.listen(PORT, function() {
