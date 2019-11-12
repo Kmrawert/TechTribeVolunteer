6d7bc10551d8e017ee189fcd84299f8199a39602
Katie Rawert <kmrawert@yahoo.com>
2019-11-11 21:13:39 -0600

    updated routes, and navigation bar

--- client/src/components/EventForm/index.js
+++ client/src/components/EventForm/index.js
@@ -3,7 +3,9 @@ import React, { Component } from "react";
 class EventForm extends Component {
     render() {
         return (
+            <div> 
             <h1>Event Form!!!!!!!!!!!!!!!</h1>
+        </div>
         )
     } 
 }
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,25 +3,17 @@ import "./style.css";
 
 function Nav() {
   return (
+    <div>
     <nav className="navbar sticky-top navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">
-        Community Connect 
-      </a>
+      <a className="navbar-brand" href="/Home"> Community Connect </a>
       <div className="navButtons">
-      <a className="navbar-brand" href="/">
-        Sign Up
-      </a>
-      <a className="navbar-brand" href="/">
-        Login 
-      </a>
-      <a className="navbar-brand" href="/">
-        Create Event
-      </a>
-      <a className="navbar-brand" href="/">
-        Volunteer
-      </a>
+      <a className="navbar-brand" href="/Login"> Sign Up </a>
+      <a className="navbar-brand" href="/Login"> Login </a>
+      <a className="navbar-brand" href="/EventForm"> Create Event </a>
+      <a className="navbar-brand" href="/Volunteer"> Volunteer </a>
       </div>
     </nav>
+    </div>
   );
 }
 
