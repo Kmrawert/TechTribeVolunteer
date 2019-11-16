d0920c5a194f7934e58d0184d689e65ca0b8b5c3
trevor orourke <trevororourke8@gmail.com>
2019-11-16 13:07:12 -0600

    added a table to hold results on homepage

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,8 +4,8 @@ export default {
   getEvents: function() {
     return axios.post("/api/events");
   },
-  getSearchTerm: function() {
-    return axios.post("/api/events" + searchTerm);
-  }
+  // getSearchTerm: function() {
+  //   return axios.post("/api/events" + searchTerm);
+  // }
 };
   
